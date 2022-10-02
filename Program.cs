871987052846a07844cc40ebdcc8108c0e1d802f
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-01 23:59:51 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NumbersOf1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 00000000000000000000000000001011;
+            var result = 0;
+            var maxBits = 32;
+            for (int bit = 0; bit < maxBits; ++bit) {
+                if (((n >> bit) & 1) == 1) {
+                    ++result;
+                }
+            }   
+            Console.WriteLine(result);
+        }
+    }
+}
